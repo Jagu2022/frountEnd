f0491998d375bb8b92ea7452a05751c8c25ebb87
bharath <sunkarajagadeesh2001@gmail.com>
2024-01-10 20:42:44 +0530

    introduction of javascript

---
+++ javascript/intoduction/whatIsJavaScript.js
@@ -0,0 +1,14 @@
+//why javascript ?
+//html is not smart enough to perform any logical work apart from displaying the given data in a better way
+    //it can't do math, in the example below it simply displayed what we typed
+    //it can't figure out if someone has correctly filled out a form
+    //it can't make decisions based on how a web visitor interacts with it.
+    //basically, html lets people read text, look at pictures, watch videos and click liks to move to other web pages with more text
+    //javascript can create smart web forms that let visitors know when they've forgotten to include necessar information
+    //javascript makes elements appear, disappear, or move around a web page
+    //in short javascript lets you make your websites more engaging effective and useful
+    // to conrtrol html and css
+    // ti intract with api
+    //to generate html and css
+    //to control media
+    
